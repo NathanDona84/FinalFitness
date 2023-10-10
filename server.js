f3614f7ce2fc74dd87b62036f41218bc6240edc5
Nathan Dona <nathan84dona@gmail.com>
2023-10-10 19:25:50 -0400

    added mongodb client

--- server.js
+++ server.js
@@ -2,19 +2,27 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
+
+const MongoClient = require('mongodb').MongoClient;
+const url ='mongodb+srv://COP4331:%40COP4331bestgroup@cluster0.3oshecf.mongodb.net/?retryWrites=true&w=majority';
+const client = new MongoClient(url);
+client.connect();
+
 app.use(cors());
 app.use(bodyParser.json());
-app.use((req, res, next) =>
-{
-res.setHeader('Access-Control-Allow-Origin', '*');
-res.setHeader(
-'Access-Control-Allow-Headers',
-'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-);
-res.setHeader(
-'Access-Control-Allow-Methods',
-'GET, POST, PATCH, DELETE, OPTIONS'
-);
-next();
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PATCH, DELETE, OPTIONS'
+    );
+    next();
 });
+
+
+
 app.listen(5000);
