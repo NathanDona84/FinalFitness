5c1ac701c9f6b38fdcfa7acdec463fac511e939c
BrinkAD1 <BrinkAD1@gmail.com>
2023-10-18 16:56:23 -0400

    login page/signup page

---
+++ frontend/src/components/SignIn.js
@@ -0,0 +1,71 @@
+import React from "react";
+function SignInForm() {
+    var loginName;
+    var loginPassword;
+    const [message, setMessage] = React.useState('');
+    const [state, setState] = React.useState({
+        email: "",
+        password: ""
+    });
+
+    const doLogin = async event => {
+        event.preventDefault();
+        console.log("inside dologin")
+        var obj = { login: loginName.value, password: loginPassword.value };
+        console.log("obj is " + obj.login + obj.password);
+        var js = JSON.stringify(obj);
+        try {
+            const response = await fetch('http://localhost:5000/api/login',
+                {
+                    method: 'POST', body: js, headers: {
+                        'Content-Type':
+                            'application/json'
+                    }
+                });
+            var res = JSON.parse(await response.text());
+            console.log(`result is ${res}`);
+            if (res.id <= 0) {
+                setMessage('User/Password combination incorrect');
+            }
+            else {
+                var user =
+                    { firstName: res.firstName, lastName: res.lastName, id: res._id }
+                    console.log(user.id)
+                localStorage.setItem('user_data', JSON.stringify(user));
+                setMessage('');
+                window.location.href = '/cards';
+            }
+        }
+        catch (e) {
+            alert(e.toString());
+            return;
+        }
+    };
+
+    return (
+        <div className="form-container sign-in-container">
+            <form onSubmit={doLogin}>
+                <h1>Sign in</h1>
+                <span>or use your account</span>
+                <input
+                    type="text"
+                    placeholder="Email"
+                    name="email"
+                    id="loginName"
+                    ref={(c) => loginName = c}
+                />
+                <input
+                    type="password"
+                    name="password"
+                    d="loginPassword" 
+                    placeholder="Password"
+                    ref={(c) => loginPassword = c}
+                />
+                <a href="#">Forgot your password?</a>
+                <button onClick={doLogin}>Sign In</button>
+            </form>
+        </div>
+    );
+}
+
+export default SignInForm;
---
+++ frontend/src/components/SignUp.js
@@ -0,0 +1,83 @@
+import React from "react";
+function SignUpForm() {
+
+    var email;
+    var password;
+    var firstName;
+    var lastName;
+
+    const [message, setMessage] = React.useState('');
+
+    const doSignUp = async event => {
+        event.preventDefault();
+        console.log("inside doSignUp")
+        var obj = {
+            Login: email.value, Password: password.value, FirstName: firstName.value,
+            LastName: lastName.value, UserId: Date.now()
+        };
+        console.log(`login: ${obj.Login} password: ${obj.Password} userID: ${obj.UserId}`);
+        var js = JSON.stringify(obj);
+        console.log(`JS: ${js}`);
+        try {
+            const response = await fetch('http://localhost:5000/api/signup',
+                {
+                    method: 'POST', body: js, headers: {
+                        'Content-Type':
+                            'application/json'
+                    }
+                });
+            var res = JSON.parse(await response.text());
+            console.log(res);
+            if (res.id <= 0) {
+                setMessage('User/Password combination incorrect');
+            }
+            else {
+                var user =
+                    { firstName: res.firstName, lastName: res.lastName, id: res.id }
+                localStorage.setItem('user_data', JSON.stringify(user));
+                setMessage('');
+                //window.location.href = '/cards';
+            }
+        }
+        catch (e) {
+            alert(e.toString());
+            return;
+        }
+    };
+
+    return (
+        <div className="form-container sign-up-container">
+            <form onSubmit={doSignUp}>
+                <h1>Create Account</h1>
+                <span>or use your email for registration</span>
+                <input
+                    type="text"
+                    name="fname"
+                    ref={(c) => firstName = c}
+                    placeholder="First Name"
+                />
+                <input
+                    type="text"
+                    name="lname"
+                    ref={(c) => lastName = c}
+                    placeholder="Last Name"
+                />
+                <input
+                    type="email"
+                    name="email"
+                    ref={(c) => email = c}
+                    placeholder="Email"
+                />
+                <input
+                    type="password"
+                    name="password"
+                    ref={(c) => password = c}
+                    placeholder="Password"
+                />
+                <button onClick={doSignUp}>Sign Up</button>
+            </form>
+        </div>
+    );
+}
+
+export default SignUpForm;
