459853f671561e28e6cc5800858de6715ea77557
Nathan Dona <nathan84dona@gmail.com>
2023-10-18 20:16:30 -0400

    login/register apis working

--- server.js
+++ server.js
@@ -23,6 +23,60 @@ app.use((req, res, next) => {
     next();
 });
 
+app.post('/api/login', async (req, res, next) => {
+    // incoming: login, password
+    // outgoing: id, firstName, lastName, error
+    let error = '';
+    const { email, password } = req.body;
+    try{
+        const db = client.db("FinalFitness");
+        const results = await db.collection('users').find({ email: email, password: password }).toArray();
+    }
+    catch(e) {
+        error = e.toString();
+    }
+
+    let id = -1;
+    let fn = '';
+    let ln = '';
+    if (results.length > 0) {
+        id = results[0]['id'];
+        fn = results[0]['firstName'];
+        ln = results[0]['lastName'];
+    }
+    let ret = { id: id, firstName: fn, lastName: ln, error: '' };
+    res.status(200).json(ret);
+});
+
+app.post('/api/register', async (req, res, next) => {
+    // incoming: UserID, Password, FirstName, LastName
+    // outgoing: error
+    const { firstName, lastName, email, password } = req.body;
+    const newUser = { "firstName": firstName, "lastName": lastName, "email": email, "password": password, "tracked": {}};
+    let error = '';
+    let inserted = 1;
+    try {
+        const db = client.db("FinalFitness");
+        let temp = await db.collection('users').find({"email": email}).toArray();
+        if(temp.length > 0){
+            inserted = -1;
+            error = 'email already exists';
+        }
+        else{
+            let id = await db.collection('users').find().sort({"id": -1}).limit(1).toArray();
+            id = id[0]["id"];
+            newUser["id"] = id+1;
+            const result = db.collection('users').insertOne(newUser);
+        }
+    }
+    catch(e){
+        inserted = -1;
+        error = e.toString();
+    }
+    //console.log(newUser);
+    let ret = {inserted: inserted, error: error};
+    res.status(200).json(ret);
+});
 
 
 app.listen(5000);
